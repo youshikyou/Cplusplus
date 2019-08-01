9442b37d68ffe5370bc66ecbeb9291b96b43d6ba
youshikyou <35782965+youshikyou@users.noreply.github.com>
2019-08-01 14:10:46 +0200

    Create Move.h

---
+++ Chapter 10/10.6/Move.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef MOVE_H_
+#define MOVE_H_
+
+#include <iostream>
+
+class Move
+	{
+	private:
+		double x;
+		double y;
+
+	public:
+		Move(double a=0.0, double b = 0.0);
+		void showmove() const;
+		Move add(const Move&m) const;
+		void reset(double a = 0.0, double b = 0.0);
+
+	};
+
+#endif //
+
