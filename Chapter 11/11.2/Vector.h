cd631ffa5a8a46aefb0fdfb73bb5fd07404348c7
youshikyou <35782965+youshikyou@users.noreply.github.com>
2019-08-08 14:47:22 +0200

    Update Vector.h

--- Chapter 11/11.2/Vector.h
+++ Chapter 11/11.2/Vector.h
@@ -3,33 +3,41 @@
 #ifndef VECTOR_H_
 #define VECTOR_H_
 #include <iostream>
+#include<fstream>
 
 namespace VECTOR
 {
 	class Vector
 	{
-		
+	public:
+		enum Mode { RECT, POL };
+
 	private:
 		double x;
 		double y;
+		Mode mode;
+		void set_x(double n1,double n2);
+		void set_y(double n1,double n2);
 
 	public:
 		Vector();
-		Vector(double n1, double n2);
+		Vector(double n1, double n2, Mode form = RECT);
 		~Vector();
-		void reset(double n1, double n2);
+		void reset(double n1, double n2, Mode form);
 		double xval()const { return x; }
 		double yval()const { return y; }
-		void magval()const;
-		void angval()const;
+		double magval() const;
+		double anval() const;
 
+		void polar_mode();
+		void rect_mode();
 		Vector operator+(const Vector &b)const;
 		Vector operator-(const Vector &b)const;
 		Vector operator-() const;
 		Vector operator *(double n) const;
 		friend Vector operator *(double n, const Vector &a);
 		friend std::ostream & operator <<(std::ostream&os, const Vector&v);
-		
+		//friend std::fstream & operator <<(std::fstream&of, const Vector&v);
 	};
 
 }
