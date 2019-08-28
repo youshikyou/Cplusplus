83d3706033d60614cf0c9b784f114a0836c9a986
youshikyou <35782965+youshikyou@users.noreply.github.com>
2019-08-28 13:29:57 +0200

    Update project1.cpp

--- Chapter 12/12.1/project1.cpp
+++ Chapter 12/12.1/project1.cpp
@@ -7,12 +7,16 @@ int main() {
 	Cow deyuan("deyuan chen", "tennis", 75);
 	std::cout << "deyuan" << "\n";
 	deyuan.ShowCow();
-	Cow zhiqiang (deyuan);
+	Cow zhiqiang =deyuan; //复制
 	std::cout << "zhiqiang" << "\n";
 	zhiqiang.ShowCow();
 
+	kaige = zhiqiang;//重载赋值运算符
+	std::cout << "kaige" << "\n";
+	kaige.ShowCow();
 
 
 	return 0;
 }
+
 
