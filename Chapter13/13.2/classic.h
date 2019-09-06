250a438ba4472079cbd98385bb375202fa3e3df2
youshikyou <35782965+youshikyou@users.noreply.github.com>
2019-09-06 14:32:33 +0200

    Create classic.h

---
+++ Chapter13/13.2/classic.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef CLASSIC_H_
+#define CLASSIC_H_
+#include"cd.h"
+
+class Classic:public Cd 
+{
+private:
+	char *mainwork;
+
+public:
+	Classic(const char * s1,const char*s2, const char * s3, int n, double x);
+	Classic();
+	virtual ~Classic();
+	virtual void Report()const;
+	Classic &operator =(const Classic&d);
+};
+
+#endif // !CLASSIC_H_
